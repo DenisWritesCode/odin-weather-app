5a0b87112fd5928fb13fdfe49dfd1eb41be65192
DenisWritesCode <deniswritescode@gmail.com>
2021-05-02 23:14:37 +0200

    Default city

--- src/index.js
+++ src/index.js
@@ -1,32 +1,26 @@
-import fetchData, { fetchImage } from "./api";
+import fetchData from "./api";
 import cleanedData from "./handleJSON";
 import pageLayout, { fillPage } from "./createPage";
 
-pageLayout();
+function conductFetch(city, units = "metric") {
+  fetchData(city, units).then((rawData) => {
+    const data = cleanedData(rawData);
+    fillPage(data);
+  });
+}
 
-const icon = document.createElement("img");
+// Quick page initialisation
+pageLayout();
+conductFetch("Johannesburg");
 
 const form = document.querySelector("form");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  let data;
   const city = document.querySelector("#citySearch");
-  fetchData(city.value, "metric").then((rawData) => {
-    data = cleanedData(rawData);
-    fillPage(data);
+  const units = document.querySelector("#units");
 
-    city.value = "";
-  });
+  conductFetch(city.value, units.value);
+
+  city.value = "";
 });
-
-// Metric for km & celsius
-// const response = fetchData("nairobi", "metric").then((rawData) => {
-//   const data = cleanedData(rawData);
-//   console.table(data);
-//   fetchImage(data.icon).then((imgURL) => {
-//     icon.src = imgURL;
-//   });
-// });
-
-document.body.appendChild(icon);
